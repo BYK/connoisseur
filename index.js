ad1313a7cda0edfbde4d9686f2175cee6ffbafac
Onur Temizkan <onurtemizkan@gmail.com>
2017-03-30 15:25:34 +0300

    Add stdin support (#6)

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
+/* eslint-disable no-console */
+
 "use strict";
 
 const connoisseur = require("./lib/connoisseur");
 const fs = require("fs");
-const sourceFiles = require("yargs").argv._;
+const concat = require("concat-stream");
+const argv = require("yargs").boolean("f").argv;
+const useFile = argv.f;
 
 const fixIndentation = code => code.replace(/(\r\n|\n|\r)/gm, "\n\t");
 
-const displayErrors = function (errors) {
-    errors.forEach(fileInfo => {
-        /* eslint-disable no-console */
-        console.log(
-            `Errors found for file ${fileInfo.path}:\n${
-                fileInfo.errors.reduce((prev, errorData) =>
-                    `${prev}\t${fixIndentation(errorData.code)}\t${
-                        errorData.error || `→ Expected: ${errorData.expected}, Found: ${errorData.actual}`
-                    }\n`
-                , "")
-            }`
-        );
-        /* eslint-enable no-console */
-    });
+const displayFileErrors = function (fileInfo) {
+    console.log(
+        `Errors found for file ${fileInfo.path}:\n${
+            fileInfo.errors.reduce((prev, errorData) =>
+                `${prev}\t${fixIndentation(errorData.code)}\t${
+                    errorData.error || `→ Expected: ${errorData.expected}, Found: ${errorData.actual}`
+                }\n`
+            , "")
+        }`
+    );
 };
 
-const errors = sourceFiles
-    .map(filePath => ({
+if (useFile) {
+    const sourceFiles = argv._;
+
+    const files = sourceFiles.filter(filePath => {
+        if (fs.existsSync(filePath))
+            return true;
+
+        console.log(`File '${filePath}' doesn't exist.`);
+
+        return false;
+    });
+
+    if (!files.length) {
+        console.log("No files found.");
+        process.exitCode = 1;
+    }
+
+    const errors = files.map(filePath => ({
         path: filePath,
         errors: connoisseur(fs.readFileSync(filePath, "utf8")),
     }))
     .filter(fileInfo => fileInfo.errors.length);
 
-const exitCode = errors.length > 0 ? 1 : 0;
+    if (errors.length) {
+        errors.forEach(displayFileErrors);
+        process.exitCode = 1;
+    } else {
+        console.log("No errors found.");
+        process.exitCode = 0;
+    }
+} else {
+    process.stdin.pipe(concat({ encoding: "string" }, text => {
+        const errors = {
+            path: "STDIN",
+            errors: connoisseur(text),
+        };
 
-displayErrors(errors);
-
-process.exit(exitCode);
+        if (errors.errors.length) {
+            displayFileErrors(errors);
+            process.exitCode = 1;
+        } else {
+            console.log("No errors found.");
+            process.exitCode = 0;
+        }
+    }));
+}
